07fee15884a7854b702aad37fd4c51325fb3fe2f
leandroGmaisP <leandro@gmaisp.com.br>
2024-04-12 19:55:59 -0300

    atualziando angular para 17

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GDA
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.4.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 
+import {  PLATFORM_ID, Inject } from '@angular/core';
+
+import { isPlatformBrowser } from "@angular/common";
+
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `<div></div>`,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'GDA';
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
-  ngOnInit(): void {
-    initFlowbite();
+   ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      initFlowbite();
+    }
   }
 }
